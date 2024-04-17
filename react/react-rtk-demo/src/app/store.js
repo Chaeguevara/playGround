861d299bd7bf3249f41270c865ab9e0ace50e692
Chaeguevara <dfso2@naver.com>
2024-04-17 17:55:30 +0900

    React project setup

---
+++ react/react-rtk-demo/src/app/store.js
@@ -0,0 +1,18 @@
+import {configureStore} from '@reduxjs/toolkit';
+// const reduxLogger = require('redux-logger')
+const cakeReducer = require('../features/cake/cakeSlice')
+const icecreamReducer = require('../features/icecream/icecreamSlice')
+const userReducer = require('../features/user/userSlice')
+
+// const logger = reduxLogger.createLogger
+
+const store = configureStore({
+    reducer: {
+        cake: cakeReducer,
+        icecream: icecreamReducer,
+        user: userReducer
+    },
+    // middleware: (getDefaultMiddleWare) => getDefaultMiddleWare().concat(logger()),
+})
+
+export default store
