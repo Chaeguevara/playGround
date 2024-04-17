adebddfe0105ffdc47b623cfce1a4eca9232810c
Chaeguevara <dfso2@naver.com>
2024-04-18 08:29:22 +0900

    React redux set up

--- react/react-rtk-demo/src/App.jsx
+++ react/react-rtk-demo/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import {CakeView} from './features/cake/CakeView'
 import {IcecreamView} from './features/icecream/IcecreamView'
@@ -10,13 +8,11 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
     <div className='App'>
       <CakeView />
       <IcecreamView/>
       <UserView/>
     </div>
-    </>
   )
 }
 
