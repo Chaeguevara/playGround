a2157ea77290a38eb643de8fd200921f9912b60c
Chaeguevara <dfso2@naver.com>
2024-05-18 21:10:02 +0900

    Implement read by useRouter()

--- ThreeJS/nextjs/03-next-js-basics-and-built-in-components/custom-_app-file/pages/greet/[name].js
+++ ThreeJS/nextjs/03-next-js-basics-and-built-in-components/custom-_app-file/pages/greet/[name].js
@@ -1,15 +1,10 @@
-export async function getServerSideProps({params}){
-    const {name} = params;
-    return{
-        props:{
-            name
-        }
-    }
-}
+import { useRouter } from "next/dist/client/router";
 
 function Greet(props){
+    const {query} = useRouter();
+    console.log(query)
     return(
-        <h1>Hello, {props.name}!</h1>
+        <h1>Hello, {query.name}!</h1>
     )
 }
 export default Greet;
