2325181d3d69dc61227cc080a0791089028167fd
Chaeguevara <dfso2@naver.com>
2024-05-19 23:45:21 +0900

    Implement Other graph example

--- algorithm/6006/Lec9/graph.py
+++ algorithm/6006/Lec9/graph.py
@@ -46,30 +46,31 @@ def bfs(Adj:list,s):
     parent[s] = s # parent of start is start itself
     level = [[s]] # the first level is itself then distance 1 levelset => distance 2 level set => ...
     while 0 <len(level[-1]): # O(?)
-        print(f"{parent=}")
+        # print(f"{parent=}")
         level.append([]) #O_a(1)
         for u in level[-2]: #O(?)
             for v in Adj[u]: #O(Adj[u])
                 if parent[v] is None: #O(1)
                     parent[v] = u # O(1)
                     level[-1].append(v) #O_a(1)
-    print(level)
+    # print(level)
     return parent
 
-test_graph = [
-    [1,4,3],
-    [0],
-    [3],
-    [0,2],
-    [0]
-]
 # test_graph = [
-#     [1],
-#     [2],
+#     [1,4,3],
 #     [0],
-#     [4],
-#     [],
+#     [3],
+#     [0,2],
+#     [0]
 # ]
+test_graph = [
+    [1],
+    [0,2],
+    [1,3,4],
+    [2,5],
+    [2,5],
+    [3,4]
+]
 for i,item in enumerate(test_graph):
     for v in item:
         G.addEdge(i,v)
