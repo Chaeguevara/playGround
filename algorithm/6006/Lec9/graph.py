6e112cfbe75029fa2a86a3dad1851db48bc2f897
Chaeguevara <dfso2@naver.com>
2024-05-19 23:26:26 +0900

    Graph with visualizer

--- algorithm/6006/Lec9/graph.py
+++ algorithm/6006/Lec9/graph.py
@@ -1,3 +1,37 @@
+import networkx as nx 
+import matplotlib.pyplot as plt 
+   
+  
+# Defining a Class 
+class GraphVisualization: 
+   
+    def __init__(self): 
+          
+        # visual is a list which stores all  
+        # the set of edges that constitutes a 
+        # graph 
+        self.visual = [] 
+          
+    # addEdge function inputs the vertices of an 
+    # edge and appends it to the visual list 
+    def addEdge(self, a, b): 
+        temp = [a, b] 
+        self.visual.append(temp) 
+          
+    # In visualize function G is an object of 
+    # class Graph given by networkx G.add_edges_from(visual) 
+    # creates a graph with a given list 
+    # nx.draw_networkx(G) - plots the graph 
+    # plt.show() - displays the graph 
+    def visualize(self): 
+        G = nx.Graph() 
+        G.add_edges_from(self.visual) 
+        nx.draw_networkx(G) 
+        plt.show() 
+  
+# Driver code 
+G = GraphVisualization() 
+
 def bfs(Adj:list,s):
     """Get parent of each vertex toward 's' direction
 
@@ -29,6 +63,19 @@ test_graph = [
     [0,2],
     [0]
 ]
+# test_graph = [
+#     [1],
+#     [2],
+#     [0],
+#     [4],
+#     [],
+# ]
+for i,item in enumerate(test_graph):
+    for v in item:
+        G.addEdge(i,v)
+
+
+
 print(bfs(test_graph,0))
 
 def unweighted_shortest_path(Adj, s, t):
@@ -41,3 +88,8 @@ def unweighted_shortest_path(Adj, s, t):
         i = parent[i]
         path.append(i)
     return path[::-1]
+
+print(f'{unweighted_shortest_path(test_graph,0,2)=}')
+
+
+G.visualize() 
