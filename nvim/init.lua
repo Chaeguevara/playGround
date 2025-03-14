c94110eac423f2e6712ac2df1ce54b1f41390a02
Chaeguevara <30302919+Chaeguevara@users.noreply.github.com>
2025-03-15 00:21:54 +0900

    Add files via upload

---
+++ nvim/init.lua
@@ -0,0 +1,11 @@
+-- Set language to English
+vim.cmd('language en_US')
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+require("config.lazy")
+
+
+
+
