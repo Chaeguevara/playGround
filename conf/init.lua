9882a04c0400735479a8509c95fa0b5cc7bd956b
Chaeguevara <30302919+Chaeguevara@users.noreply.github.com>
2025-03-15 00:21:16 +0900

    Delete conf/init.lua

--- conf/init.lua
+++
@@ -1,7 +0,0 @@
--- Set language to English
-vim.cmd('language en_US')
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-require("config.lazy")
