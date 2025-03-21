04f65684e67fdd63907cc26f5fa0daee052baf7d
Chaeguevara <30302919+Chaeguevara@users.noreply.github.com>
2025-03-21 22:18:00 +0900

    Add files via upload

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,11 +1,17 @@
 -- Set language to English
 vim.cmd('language en_US')
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-require("config.lazy")
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
-
-
-
+require("vim-options")
+require("lazy").setup("plugins")
